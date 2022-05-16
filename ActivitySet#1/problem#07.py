b00b8f83543ba97c5bec37650168b9e349d3d7a0
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-05-16 17:41:06 +0000

    p7

--- ActivitySet#1/problem#07.py
+++ ActivitySet#1/problem#07.py
@@ -1,3 +1,25 @@
-# Strings
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+       num=int(num)
+    except:
+        print('invalid input')
+        continue
+    if  largest is None:
+        largest = num
+    elif num>largest:
+        largest=num
+    if smallest is None:
+        smallest=num
+    elif num<smallest:
+        smallest=num
+          
+   
+ 
 
-text = "X-DSPAM-Confidence:    0.8475"
+print("Maximum", largest)
+print("Minimum",smallest)
