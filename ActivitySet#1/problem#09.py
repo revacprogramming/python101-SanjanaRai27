0f92846e8f23dab1ba3ba23329d5650b21abcf01
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-05-24 10:11:59 +0000

    done p9

--- ActivitySet#1/problem#09.py
+++ ActivitySet#1/problem#09.py
@@ -1,3 +1,19 @@
-# Lists
 
-filename = "dataset/romeo.txt"
+# Use the file name mbox-short.txt as the file name
+
+fname = input("Enter file name: ")
+fh = open(fname)
+count=0
+total=0
+
+for line in fh:
+    if not  line.startswith("X-DSPAM-Confidence:"):
+        continue
+    else:
+        count=count+1
+        z=line.find('0')
+        y=float(line[z:])
+        total=total+y
+asc=total/count             
+        
+print("Average spam confidence:",asc)
