ba719820adfb04b22f1edd096217d4c7f98229fe
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-05-26 17:08:48 +0000

    p10 done

--- ActivitySet#1/problem#10.py
+++ ActivitySet#1/problem#10.py
@@ -1,3 +1,15 @@
-# Dictionaries
 
-filename = "dataset/mbox-short.txt"
+fname = raw_input("Enter file name: ")
+fh = open(fname)
+lst = list()
+for line in fh:
+    line=line.rstrip()
+    line= line.split()
+    for c in line:
+        if c in lst:
+           continue
+        else:
+           lst.append(c)
+            
+lst.sort()
+print(lst)
