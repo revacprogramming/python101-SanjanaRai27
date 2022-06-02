216ebbf7e8a3325eedda421b9e48aee21a71b328
sanjanaraibs27 <97089389+SanjanaRai27@users.noreply.github.com>
2022-06-02 17:34:23 +0000

    ///

--- ActivitySet#1/problem#16.py
+++ ActivitySet#1/problem#16.py
@@ -1,2 +1,25 @@
-# Databases
-# https://www.py4e.com/lessons/database
+largest = None
+smallest = None
+while True:
+    num = input("Enter a number: ")
+    if num == "done":
+        break
+    try:
+       num=int(num)
+    except:
+        print('invalid input')
+        continue
+    if  largest is None:
+        largest = num
+    elif num>largest:
+        largest=num
+    if smallest is None:
+        smallest=num
+    elif num<smallest:
+        smallest=num
+          
+   
+ 
+
+print("Maximum", largest)
+print("Minimum",smallest)
